1528120cce36439d7807e494775447f016c68ead
AbdullahAmeen <46934578+AbdullahAmeen@users.noreply.github.com>
2019-02-13 19:16:21 -0600

    Module3_Assignment

---
+++ Module3/js/main_2.js
@@ -0,0 +1,76 @@
+//initialize function called when the script loads
+function initialize(){
+    cities();
+};
+
+//function to create a table with cities and their populations
+function cities(){
+    //define two arrays for cities and population
+    var cityPop = [
+        {
+            city: 'Phoenix',
+            population: 1626078
+        },
+        {
+            city: 'Dallas',
+            population: 1341075
+        },
+        {
+            city: 'Indianapolis',
+            population: 863002
+        },
+        {
+            city: 'Boston',
+            population: 685094
+        },
+		{	city:'Chicago',
+			population:2695598
+		}
+			
+    ];
+
+    //append the table element to the div
+    $("#mydiv").append("<table>");
+
+    //append a header row to the table
+    $("table").append("<tr>");
+
+    //add the "City" and "Population" columns to the header row
+    $("tr").append("<th>City</th><th>Population</th>");
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cityPop.length; i++){
+        //assign longer html strings to a variable
+        var rowHtml = "<tr><td>" + cityPop[i].city + "</td><td>" + cityPop[i].population + "</td></tr>";
+        //add the row's html string to the table
+        $("table").append(rowHtml);
+    };
+};
+
+function debugCallback(response){
+	
+	$(mydiv).append('GeoJSON data: ' + JSON.stringify(mydata));
+};
+
+
+
+function debugAjax(){
+	
+	var mydata;
+
+	$.ajax("data/MegaCities.geojson", {
+		dataType: "json",
+		success: function(response){
+			
+			debugCallback(mydata);
+		}
+	});
+
+	$(mydiv).append('<br>GeoJSON data:<br>' + JSON.stringify(mydata));
+};
+
+$(mydiv).append('GeoJSON data: ' + JSON.stringify(mydata));
+
+
+//call the initialize function when the document has loaded
+$(document).ready(initialize);
